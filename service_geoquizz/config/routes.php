1a05b1e62abe0609a992fc997ddebd363410f54b
Alexis Chân Gridel <alexis.gridel@pm.me>
2024-02-05 16:57:19 +0100

    feat(service/web): get action by id

--- service_geoquizz/config/routes.php
+++ service_geoquizz/config/routes.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use geoquizz\service\web\action\GetGameAction;
 use geoquizz\service\web\action\ListGamesAction;
 
 return static function ($app) {
@@ -16,4 +17,6 @@ return static function ($app) {
     });
 
     $app->get('/games', ListGamesAction::class);
+
+    $app->get('/games/{id}', GetGameAction::class);
 };
