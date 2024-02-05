0747c27a4d57e2dcbb969d280e634447640ffe43
DELLINGER Agathe <agathe.dellinger2@etu.univ-lorraine.fr>
2024-02-05 13:10:30 +0100

    Landing vue + components WITHOUT css

---
+++ vue_geoquizz/src/components/Game.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+export default {
+  props: {
+    serie: {
+      type: String,
+      required: true
+    },
+    level: {
+      type: String,
+      required: true
+    },
+    photo: {
+      type: String,
+      required: true
+    }
+  }
+
+}
+</script>
+
+<template>
+  <div class="game-card">
+    <img :src="photo" alt="photo" />
+    <div class="card-body">
+      <p class="card-text">Partie sur {{serie}}</p>
+      <p class="card-text">Niveau {{level}}</p>
+    </div>
+  </div>
+
+</template>
--- vue_geoquizz/src/views/LandingVue.vue
+++ vue_geoquizz/src/views/LandingVue.vue
@@ -1,3 +1,66 @@
+<script lang="ts">
+import {RouterView} from 'vue-router'
+import Game from '@/components/Game.vue'
+
+export default {
+  components: {
+    RouterView,
+    Game
+  },
+  data() {
+    return {
+      publicGames: {
+        game1: {
+          serie: "Paris",
+          level: "Facile",
+          photo: "../assets/img/Nancy.jpg"
+        },
+        game2: {
+          serie: "Montpellier",
+          level: "Moyen",
+          photo: ""
+        },
+        game3: {
+          serie: "Nancy",
+          level: "Difficile",
+          photo: ""
+        },
+        game4: {
+          serie: "Nancy",
+          level: "Facile",
+          photo: ""
+        }
+      }
+    }
+  }
+}
+</script>
+
 <template>
-    <h1>Page d'accueil</h1>
+  <div>le header</div>
+  <h1>GeoQuizz</h1>
+  <div class="new-game">
+    <h2>Lancer une nouvelle partie</h2>
+    <div class="new-game-form">
+      <select>
+        <option value="" selected disabled>Choisir une ville</option>
+        <option value="Paris">Paris</option>
+        <option value="Montpellier">Montpellier</option>
+        <option value="Nancy">Nancy</option>
+      </select>
+      <select>
+        <option value="" selected disabled>Choisir un mode de jeu</option>
+        <option value="easy">Facile</option>
+        <option value="medium">Moyen</option>
+        <option value="hard">Difficile</option>
+      </select>
+      <button>Lancer</button>
+    </div>
+  </div>
+
+  <div class="public-games">
+    <h2>Parties publiques</h2>
+    <Game v-for="game in publicGames" :serie="game.serie" :level="game.level" :photo="game.photo"></Game>
+  </div>
+
 </template>
