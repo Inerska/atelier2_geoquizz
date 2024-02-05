6514179aab9c81dcef9bb646e2013c29ed732d30
Alexis Chân Gridel <alexis.gridel@pm.me>
2024-02-05 13:35:25 +0100

    feat(service/geoquizz): doctrine orm initial commit

---
+++ service_geoquizz/config/dependencies.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
+return function ($container) {
+    $container->set('logger', function () {
+        $logger = new Logger('my_logger');
+        $file_handler = new StreamHandler(__DIR__ . '/../logs/app.log');
+        $logger->pushHandler($file_handler);
+        return $logger;
+    });
+
+    // add doctrine entity manager
+    $container->set('em', function () {
+        $config = ORMSetup::createAttributeMetadataConfiguration([__DIR__ . '/../src/infrastructure/entity'], true);
+        try {
+            $connectionConfiguration = parse_ini_file('database.conf.ini');
+            $connection = DriverManager::getConnection($connectionConfiguration);
+
+            if ($connection === null) {
+                throw new Exception('Connection is null');
+            }
+
+            return new EntityManager($connection, $connectionConfiguration);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    });
+};
