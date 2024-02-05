663996cd977156aa41dddfb18dd3c4c12408288e
Alexis Chân Gridel <alexis.gridel@pm.me>
2024-02-05 16:59:49 +0100

    feat(service/web): fix routes

--- service_geoquizz/config/routes.php
+++ service_geoquizz/config/routes.php
@@ -6,17 +6,8 @@ use geoquizz\service\web\action\GetGameAction;
 use geoquizz\service\web\action\ListGamesAction;
 
 return static function ($app) {
-    $app->get('/', function ($request, $response, $args) {
-        $response->getBody()->write("Hello, world");
-        return $response;
-    });
-    $app->get('/hello/{name}', function ($request, $response, $args) {
-        $name = $args['name'];
-        $response->getBody()->write("Hello, $name");
-        return $response;
-    });
 
-    $app->get('/games', ListGamesAction::class);
+    $app->get('/games[/]', ListGamesAction::class);
 
-    $app->get('/games/{id}', GetGameAction::class);
+    $app->get('/games/{id}[/]', GetGameAction::class);
 };
