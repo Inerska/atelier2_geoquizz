453cd999d77899bbe7444e28961645f5df423f22
Alexis Chân Gridel <alexis.gridel@pm.me>
2024-02-05 12:47:27 +0100

    fix(service/geoquizz): autoload in index

--- service_geoquizz/config/routes.php
+++ service_geoquizz/config/routes.php
@@ -3,6 +3,10 @@
 declare(strict_types=1);
 
 return function ($app) {
+    $app->get('/', function ($request, $response, $args) {
+        $response->getBody()->write("Hello, world");
+        return $response;
+    });
     $app->get('/hello/{name}', function ($request, $response, $args) {
         $name = $args['name'];
         $response->getBody()->write("Hello, $name");
--- service_geoquizz/public/index.php
+++ service_geoquizz/public/index.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
 $app = require_once __DIR__ . '/../config/bootstrap.php';
 
 $app->run();
