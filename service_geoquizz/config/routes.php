b38775371008f42bf8242899b1ab268a2cf26ccc
Alexis Chân Gridel <alexis.gridel@pm.me>
2024-02-06 12:14:57 +0100

    feat(service): update and create initial commit

--- service_geoquizz/config/routes.php
+++ service_geoquizz/config/routes.php
@@ -2,12 +2,18 @@
 
 declare(strict_types=1);
 
+use geoquizz\service\web\action\CreateGameAction;
 use geoquizz\service\web\action\GetGameAction;
 use geoquizz\service\web\action\ListGamesAction;
+use geoquizz\service\web\action\UpdateGameAction;
 
 return static function ($app) {
 
     $app->get('/games[/]', ListGamesAction::class);
 
     $app->get('/games/{id}[/]', GetGameAction::class);
+
+    $app->put('/games/{id}[/]', UpdateGameAction::class);
+
+    $app->post('/games[/]', CreateGameAction::class);
 };
