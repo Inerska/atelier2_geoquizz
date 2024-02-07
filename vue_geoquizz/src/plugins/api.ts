ff9885f8e1528824af414c84b98052d78ae417fc
DELLINGER Agathe <agathe.dellinger2@etu.univ-lorraine.fr>
2024-02-07 17:15:37 +0100

    feat(api): Landing (tooltip + link to series) + SeriesVue start + tests API ? (Alexis)

--- vue_geoquizz/src/plugins/api.ts
+++ vue_geoquizz/src/plugins/api.ts
@@ -3,12 +3,12 @@ import axios from 'axios';
 export default {
     install: function (app) {
         app.config.globalProperties.$api = axios.create({
-            baseURL : 'http://gateway_nginx/api/v1/',
-            //baseURL : 'http://localhost:8084/api/v1/',
+            //baseURL : 'http://gateway_nginx/api/v1/',
+            baseURL : 'http://localhost:8084/api/v1/',
             headers: {
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin' : '*',
-                scheme: 'http',
+                //'Access-Control-Allow-Origin' : '*',
+                //scheme: 'http',
             }
         })
     }
