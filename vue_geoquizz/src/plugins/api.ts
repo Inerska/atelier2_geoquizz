e3d0e362d926e7b3f86d2e7d8e0ead9a8c4f36b9
DELLINGER Agathe <agathe.dellinger2@etu.univ-lorraine.fr>
2024-02-07 13:47:21 +0100

    feat(api): test fix cors

--- vue_geoquizz/src/plugins/api.ts
+++ vue_geoquizz/src/plugins/api.ts
@@ -4,8 +4,11 @@ export default {
     install: function (app) {
         app.config.globalProperties.$api = axios.create({
             baseURL : 'http://gateway_nginx/api/v1/',
+            //baseURL : 'http://localhost:8084/api/v1/',
             headers: {
                 'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin' : '*',
+                scheme: 'http',
             }
         })
     }
